7d8e448bc3bb60608858f5fd0ab90474ba32fca8
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-04 19:32:23 -0400

    adds recipes router to extract code from server.js file

---
+++ recipesRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyparser = require('body-parser');
+const jsonparser = bodyparser.json();
+
+
+
+module.exports = router;
